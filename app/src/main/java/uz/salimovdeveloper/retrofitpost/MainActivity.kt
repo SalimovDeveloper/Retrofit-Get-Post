632ac2a77edef979227b2dac7947d31298e474f0
salimovdeveloper <salimovdeveloper@gmail.com>
2022-11-19 14:42:34 -0600

    toliq ishlashga tayyor

--- app/src/main/java/uz/salimovdeveloper/retrofitpost/MainActivity.kt
+++ app/src/main/java/uz/salimovdeveloper/retrofitpost/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import uz.salimovdeveloper.retrofitpost.adapters.RvAdapter
 import uz.salimovdeveloper.retrofitpost.databinding.ActivityMainBinding
 import uz.salimovdeveloper.retrofitpost.databinding.ItemDialogBinding
 import uz.salimovdeveloper.retrofitpost.models.MyTodoGetResponse
@@ -17,6 +18,7 @@ import uz.salimovdeveloper.retrofitpost.retrofit.ApiClient
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var rvAdapter: RvAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,6 +28,39 @@ class MainActivity : AppCompatActivity() {
         getData()
 
         postData()
+    }
+
+    private fun getData() {
+        ApiClient.getRetrofitService().getAllTodo()
+            .enqueue(object : Callback<List<MyTodoGetResponse>> {
+                override fun onResponse(
+                    call: Call<List<MyTodoGetResponse>>,
+                    response: Response<List<MyTodoGetResponse>>
+                ) {
+                    if (response.isSuccessful){
+                        rvAdapter = RvAdapter(response.body()!!)
+                        binding.myRecyc.adapter = rvAdapter
+                    }
+                }
+
+                override fun onFailure(call: Call<List<MyTodoGetResponse>>, t: Throwable) {
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Internet bilan muammo!!!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.my_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        postData()
+        return super.onOptionsItemSelected(item)
     }
 
     private fun postData() {
@@ -43,12 +78,12 @@ class MainActivity : AppCompatActivity() {
                 )
 
                 ApiClient.getRetrofitService().addAllTodo(myTodoPostRequest)
-                    .enqueue(object :Callback<MyTodoGetResponse>{
+                    .enqueue(object : Callback<MyTodoGetResponse> {
                         override fun onResponse(
                             call: Call<MyTodoGetResponse>,
                             response: Response<MyTodoGetResponse>
                         ) {
-                            if (response.isSuccessful){
+                            if (response.isSuccessful) {
                                 Toast.makeText(
                                     this@MainActivity,
                                     "${response.body()?.sarlavha} saqlandi",
@@ -72,16 +107,5 @@ class MainActivity : AppCompatActivity() {
         dialog.show()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.my_menu, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        postData()
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun getData() {
-    }
 }
