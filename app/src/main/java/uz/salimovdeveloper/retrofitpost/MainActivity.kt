6f522efdd57d24bca8ed042c1966db0b470c20ee
salimovdeveloper <salimovdeveloper@gmail.com>
2022-11-19 14:46:13 -0600

    Tayyor

--- app/src/main/java/uz/salimovdeveloper/retrofitpost/MainActivity.kt
+++ app/src/main/java/uz/salimovdeveloper/retrofitpost/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : AppCompatActivity() {
 
         getData()
 
-        postData()
     }
 
     private fun getData() {
@@ -90,6 +89,7 @@ class MainActivity : AppCompatActivity() {
                                     Toast.LENGTH_SHORT
                                 ).show()
                                 getData()
+                                dialog.cancel()
                             }
                         }
 
